27d4e912b3f11ccd3dd7c68f6ee30cb4254a7a23
YeungBrandon <BBGgaming0917@gmail.com>
2026-08-31 17:28:47 +0800

    Local files

--- android/app/src/main/java/com/stpauls/progmosis/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.stpauls.progmosis;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.stpauls.progmosis',
-  appName: 'ProgMOSis',
-  webDir: 'dist'
-};
-
-export default config;
